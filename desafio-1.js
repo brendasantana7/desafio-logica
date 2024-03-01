b6515e0e28f1ead285175dab0984465f08342380
Brenda Santana <breendasantaana@gmail.com>
2024-03-01 20:55:05 -0300

    finish

---
+++ desafio-1.js
@@ -0,0 +1,31 @@
+let nomeHeroi = "Minerva000"
+let nivelXP = 
+
+
+let mensagem;
+
+if (nivelXP <= 1000){
+    mensagem = "O Herói de nome "+ nomeHeroi + " está no nível de Ferro"
+} 
+else if (nivelXP >= 1001 && nivelXP <=2000){
+    mensagem = "O Herói de nome "+ nomeHeroi + " está no nível de Bronze" 
+} 
+else if (nivelXP >= 2001 && nivelXP <=5000){
+    mensagem = "O Herói de nome "+ nomeHeroi + " está no nível de Prata"
+} 
+else if (nivelXP >= 6001 && nivelXP <=7000){
+    mensagem = "O Herói de nome "+ nomeHeroi + " está no nível de Ouro"
+} 
+else if (nivelXP >= 7001 && nivelXP <=8000){
+    mensagem = "O Herói de nome "+ nomeHeroi + " está no nível de Platina"
+} 
+else if (nivelXP >= 8001 && nivelXP <=9000){
+    mensagem = "O Herói de nome "+ nomeHeroi + " está no nível de Ascendente"
+}
+else if (nivelXP >= 9001 && nivelXP <=10000){
+    mensagem = "O Herói de nome "+ nomeHeroi + " está no nível de Imortal"
+}
+else if (nivelXP >= 10001){
+    mensagem = "O Herói de nome "+ nomeHeroi + " está no nível de Radiante"
+}
+console.log(mensagem)
